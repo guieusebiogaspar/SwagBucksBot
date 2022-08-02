e11c09cd2f5654c99cc201ce1009f75aecaedf39
midhunvnadh <midhun.vnadh5732@gmail.com>
2022-08-02 18:21:12 +0530

    Maintain for new code

--- swag.py
+++ swag.py
@@ -1,39 +1,75 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from time import sleep
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 import random
+import pickle
+
 
 class SwagBot:
     def __init__(self, username, pw):
         #self.driver = webdriver.Chrome()
-        self.driver = webdriver.Chrome(ChromeDriverManager().install())
-        self.username = username
-        self.pw = pw
+        self.driver = webdriver.Chrome(
+            service=Service(ChromeDriverManager().install())
+        )
         self.driver.get("https://www.swagbucks.com/p/login")
-        sleep(2)
-        self.driver.find_element_by_xpath("//input[@name=\"emailAddress\"]").send_keys(username)
-        self.driver.find_element_by_xpath("//input[@name=\"password\"]").send_keys(pw)
-        sleep(2)
-        self.driver.find_element_by_xpath('//button[@id="loginBtn"]').click()
-        sleep(100) # time for you to do the thing that proves you are not a bot
+        try:
+            cookies = pickle.load(open("cookies.pkl", "rb"))
+            for cookie in cookies:
+                self.driver.add_cookie(cookie)
+        except:
+            self.username = username
+            self.pw = pw
+            sleep(2)
+            self.driver.find_element_by_xpath(
+                "//input[@name=\"emailAddress\"]").send_keys(username)
+            self.driver.find_element_by_xpath(
+                "//input[@name=\"password\"]").send_keys(pw)
+            login_btn = self.driver.find_element(By.ID, "loginBtn")
+            login_btn.click()
+            sleep(5)
+            pickle.dump(self.driver.get_cookies(), open("cookies.pkl", "wb"))
+            sleep(5)
 
-    def survey(self):
-        self.driver.find_element_by_xpath("//a[contains(@href,'/surveys')]").click()
+    def startSurvey(self):
+        self.driver.get("https://www.swagbucks.com/surveys")
         sleep(2)
-        for i in range(1000):
-            x = random.randint(1,2)
-            y = random.randint(1,4)
-            sleep(y)
-            self.driver.find_element_by_class_name("surveyDropdownVal").click()
-            sleep(x)
-            string1 = "/html/body/div[4]/div[2]/div[1]/div[1]/div[1]/div[2]/div[2]/div[2]/div/div[2]/div/div[1]/div/div/span[" + str(x) + "]"
-            self.driver.find_element_by_xpath(string1).click()
-            sleep(y)
-            #self.driver.find_element_by_xpath('//button[@class=surveyDashboardCTA sbCta sbBgPrimaryColor"]')
-            self.driver.find_element_by_xpath("/html/body/div[4]/div[2]/div[1]/div[1]/div[1]/div[2]/div[2]/div[2]/div/div[2]/div/div[2]/button").click()
-            #self.driver.find_elements_by_class_name("surveyDashboardCTA sbCta sbBgPrimaryColor").click()
-            sleep(x)
-            print(i)
-            
-my_bot = SwagBot(*your username*, *your password*)
-my_bot.survey()
+        try:
+            while True:
+                x = random.randint(1, 2)
+                y = random.randint(1, 4)
+                sleep(y)
+                dropDown = self.driver.find_elements(
+                    By.CSS_SELECTOR, (".surveyDropdownVal")
+                )
+                if(len(dropDown) > 0):
+                    dropDown[0].click()
+                else:
+                    print("Dropdown not found! Trying in 5 mins")
+                    sleep(300)
+                    self.driver.refresh()
+                    continue
+                sleep(x)
+
+                options = self.driver.find_elements(By.CSS_SELECTOR, (
+                    ".questionDropdownOptions > span")
+                )
+
+                random.choice(options).click()
+
+                sleep(x)
+
+                self.driver.find_element(By.CSS_SELECTOR, (
+                    ".surveyDashboardCTA")
+                ).click()
+
+                sleep(x)
+        except Exception as e:
+            print(e)
+            print("Will try later")
+            sleep(60 * 60 * 1)
+
+
+my_bot = SwagBot("midhun.vnadh5732@gmail.com", "pe3&+&k3_$S8CzQ")
+my_bot.startSurvey()
